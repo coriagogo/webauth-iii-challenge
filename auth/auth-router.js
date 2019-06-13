0745d0a149762a5d751e32dd5c1cbc452fbf54aa
coriagogo <pkcparis@gmail.com>
2019-06-13 17:32:01 -0400

    updated users-model.js to include restricted middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,7 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username})
+  Users.findBy({ username })
     .first()
     .then(user => {
       if(user && bcrypt.compareSync(password, user.password)) {
